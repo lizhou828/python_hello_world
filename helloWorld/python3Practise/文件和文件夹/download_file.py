6b0f62a738127b6506dbe37eb9689d27ea84902f
lizhou828 <lizhou828@126.com>
2019-06-10 14:56:46 +0800

    下载文件 并 保存到新文件中

--- helloWorld/python3Practise/文件和文件夹/download_file.py
+++ helloWorld/python3Practise/文件和文件夹/download_file.py
@@ -12,14 +12,14 @@
 
 """
 
-import urllib
+from urllib.request import urlretrieve
 import requests
+
 url = 'http://www.blog.pythonlibrary.org/wp-content/uploads/2012/06/wxDbViewer.zip'
 print("downloading with urllib")
-urllib.urlretrieve(url, "code.zip")
-
+urlretrieve(url, "code.zip")
 
 print("downloading with requests")
 r = requests.get(url)
 with open("code3.zip", "wb") as code:
-     code.write(r.content)
+    code.write(r.content)
