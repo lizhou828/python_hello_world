0944ec0e9807f33980cf25de3dbd9a0861fb991a
lizhou828 <lizhou828@126.com>
2019-06-10 14:52:10 +0800

    下载文件 并 保存到新文件中

---
+++ helloWorld/python3Practise/文件和文件夹/download_file.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+
+"""
+@Function: 下载文件 并 保存到新文件中
+@File    :          download.py    
+@Contact :          lizhou@glorypty.com
+@License :          (C)Copyright 2019-2020
+
+@Modify Time        @Author      @Version        @Desciption
+------------        -------      --------        -----------
+2019-6-10 14:50     lizhou         1.0         
+
+"""
+
+import urllib
+import requests
+url = 'http://www.blog.pythonlibrary.org/wp-content/uploads/2012/06/wxDbViewer.zip'
+print("downloading with urllib")
+urllib.urlretrieve(url, "code.zip")
+
+
+print("downloading with requests")
+r = requests.get(url)
+with open("code3.zip", "wb") as code:
+     code.write(r.content)
