797a4aa0f266a6461875cc4af4c990cf6d8188b1
lizhou828 <lizhou828@126.com>
2019-08-06 11:23:24 +0800

    python3： django入门教程

---
+++ helloWorld/django_learning/Readme.md
@@ -0,0 +1,4 @@
+# django入门教程
+
+## 参考文档
+    https://www.runoob.com/django/django-tutorial.html
---
+++ helloWorld/django_learning/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+"""
+@Function:
+@File    :          __init__.py.py    
+@Contact :          lizhou@glorypty.com
+@License :          (C)Copyright 2019-2020
+
+@Modify Time        @Author      @Version        @Desciption
+------------        -------      --------        -----------
+2019-8-6 11:01     lizhou         1.0         
+
+"""
