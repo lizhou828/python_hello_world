af7c789bc30a2b2f98b738ad73b83fdd46942cc4
lizhou828 <lizhou@glorypty.com>
2019-10-28 15:07:41 +0800

    阿里云oss上传

--- helloWorld/aliyun_oss_upload/test1.py
+++ helloWorld/aliyun_oss_upload/test1.py
@@ -23,7 +23,7 @@ print(upload_file_path)
 bucket.put_object_from_file(key= upload_file_path , filename='E:/安装的软件.png')
 
 # 3、访问文件
-url = bucket.sign_url('GET', upload_file_path, 5 * 60)
+url = bucket.sign_url('GET', upload_file_path, 100*3600)
 print("文件上传成功，返回的url是:\n"+ url)
 # http://test123456788.oss-cn-shenzhen.aliyuncs.com/land_report%2Ffirst_land%2F2019-10%2F%E5%AE%89%E8%A3%85%E7%9A%84%E8%BD%AF%E4%BB%B6.png?OSSAccessKeyId=LTAI4Fp6PCsntQ5ooFhFf3du&Expires=1572244135&Signature=JKN9RyhWQmBZPE3vedVWzVWPpHE%3D
 # http://test123456788.oss-cn-shenzhen.aliyuncs.com/land_report%2Ffirst_land%2F2019-10%2F%E5%AE%89%E8%A3%85%E7%9A%84%E8%BD%AF%E4%BB%B6.png?OSSAccessKeyId=LTAI4Fp6PCsntQ5ooFhFf3du&Expires=1572244184&Signature=5Vhkaw%2BCAa8st2952NmQ7sLZ9WI%3D
@@ -35,7 +35,6 @@ bucket.get_object_to_file(upload_file_path, 'E:/我是下载的安装的软件.p
 
 
 
-
 #5、删除文件
-# bucket.delete_object(upload_file_path)
+bucket.delete_object(upload_file_path)
 
