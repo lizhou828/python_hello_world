938513ca0547bfd1c6c398d92868e2a34bedf19e
lizhou828 <lizhou828@126.com>
2019-11-17 16:54:58 +0800

    pip安装路径查询

--- helloWorld/jinja2_db/template/service/{class_name}Dao.py
+++ helloWorld/jinja2_db/template/service/{class_name}Dao.py
@@ -1,5 +1,6 @@
 # 引入同一目录下的py文件（来自指定py文件下，导入该文件下的某个类）
 
+from {{package_name}}.DB.BaseDao import BaseDao
 from {{package_name}}.model.{{class_name}} import {{class_name}}
 
 class {{class_name}}Dao(BaseDao):
