fc934ae17cb957f716aaa13070b3ebb56eafde2a
lizhou828 <lizhou828@126.com>
2019-11-18 22:03:23 +0800

    1、对象转dict 2、jinja2的指令不占用行

--- helloWorld/jinja2_db/template/model/{class_name}.py
+++ helloWorld/jinja2_db/template/model/{class_name}.py
@@ -20,14 +20,40 @@ class {{class_name}}(object):
         self.__{{column.name}} = {{column.name}}
     {% endfor %}
 
+    def keys(self):
+        '''
+        当对实例化对象使用dict(obj)的时候, 会调用这个方法,这里定义了字典的键, 其对应的值将以obj['name']的形式取,
+        但是对象是不可以以这种方式取值的, 为了支持这种取值, 可以为类增加一个方法
+        :param self:
+        :return:
+        '''
+        return (
+                   {%- for column in columns -%}
+                        {%- if loop.first -%}
+                            '{{column.name}}'
+                        {%- else -%}
+                            , '{{column.name}}'
+                        {%-endif -%}
+                   {%- endfor -%}
+        )
+
+    def __getitem__(self, item):
+        '''
+        内置方法,当对实例化对象使用dict(obj)的时候, 当使用obj['name']的形式的时候, 将调用这个方法, 这里返回的结果就是值
+        :param self:
+        :param item:
+        :return:
+        '''
+        return getattr(self, item)
+
     def __str__(self):
         {{class_name}}Dict = {
-            {% for column in columns %}
-                {% if loop.first %}
+            {%- for column in columns -%}
+                {%- if loop.first -%}
                     '{{column.name}}': self.__{{column.name}}
-                {%else%}
+                {%-else-%}
                     ,'{{column.name}}': self.__{{column.name}}
-                {%endif%}
-            {% endfor %}
+                {%-endif-%}
+            {%- endfor -%}
         }
         return json.dumps({{class_name}}Dict,cls=DateEncoder)
--- helloWorld/python3Practise/ReadMe.md
+++ helloWorld/python3Practise/ReadMe.md
@@ -1,26 +1,29 @@
-# 《零基础学python》（第二版）
-https://docs.pythontab.com/learnpython/
+# 参考文档
 
-Python 黑魔法（持续收录）
-https://www.cnblogs.com/crackpotisback/p/6652247.html
+[《零基础学python》（第二版）](https://docs.pythontab.com/learnpython/)
 
-python3和Python2的区别（被坑太久了）
-https://www.cnblogs.com/hanggegege/p/5840005.html
 
-Python的35种“黑魔法”级别技巧！
- https://www.cnblogs.com/ittop/p/9262105.html
+[Python 黑魔法（持续收录）](https://www.cnblogs.com/crackpotisback/p/6652247.html)
 
-python3中的奇怪语法
-https://www.cnblogs.com/qqhexi/p/7305225.html
 
-python 的一些高级编程技巧
-https://www.cnblogs.com/wangjian8888/p/6111701.html
+[python3和Python2的区别（被坑太久了）](https://www.cnblogs.com/hanggegege/p/5840005.html)
 
-安利一波Python编程中非常实用的那些黑魔法
-https://www.jianshu.com/p/0611a2bbbd8d
 
-Python3实用编程技巧进阶一
-https://www.cnblogs.com/sunlong88/articles/10252780.html
+[Python的35种“黑魔法”级别技巧！](https://www.cnblogs.com/ittop/p/9262105.html)
 
-Python高效编程的19个技巧
-https://www.cnblogs.com/rourou1/p/6148483.html
+[python3中的奇怪语法](https://www.cnblogs.com/qqhexi/p/7305225.html)
+
+
+[python 的一些高级编程技巧](https://www.cnblogs.com/wangjian8888/p/6111701.html)
+
+
+[安利一波Python编程中非常实用的那些黑魔法](https://www.jianshu.com/p/0611a2bbbd8d)
+
+
+[Python3实用编程技巧进阶一](https://www.cnblogs.com/sunlong88/articles/10252780.html)
+
+
+[Python高效编程的19个技巧](https://www.cnblogs.com/rourou1/p/6148483.html)
+
+
+[Python3 hasattr()、getattr()、setattr()、delattr()函数](https://www.cnblogs.com/hwlong/p/8871661.html)
