1a91f521f0c5a0b8b2e8dd438c94d8787ef9d551
lizhou828 <lizhou@glorypty.com>
2019-11-19 09:56:35 +0800

    把实例下的私有属性 改成实例属性

--- helloWorld/jinja2_db/template/model/{class_name}.py
+++ helloWorld/jinja2_db/template/model/{class_name}.py
@@ -6,18 +6,18 @@ class {{class_name}}(object):
     def __init__(self):
     {% for column in columns %}
         # {{column.comment}}
-        self.__{{column.name}} = None
+        self.{{column.name}} = None
     {% endfor %}
 
     {% for column in columns %}
     def get_{{column.name}}(self):
         # {{column.comment}}
-        return self.__{{column.name}}
+        return self.{{column.name}}
     {% endfor %}
     {%for column in columns %}
     def set_{{column.name}}(self, {{column.name}}):
         # {{column.comment}}
-        self.__{{column.name}} = {{column.name}}
+        self.{{column.name}} = {{column.name}}
     {% endfor %}
 
     def keys(self):
@@ -50,9 +50,9 @@ class {{class_name}}(object):
         {{class_name}}Dict = {
             {%- for column in columns -%}
                 {%- if loop.first -%}
-                    '{{column.name}}': self.__{{column.name}}
+                    '{{column.name}}': self.{{column.name}}
                 {%-else-%}
-                    ,'{{column.name}}': self.__{{column.name}}
+                    ,'{{column.name}}': self.{{column.name}}
                 {%-endif-%}
             {%- endfor -%}
         }
